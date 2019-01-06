7037346f653ec1e73d759ca2e96f9b1d045d58b3
Amari Ellington <amari.l.ellington@gmail.com>
2019-01-06 14:01:17 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,4 +2,4 @@ var chocolateBars = ["snickers","hundred grand","kitkat","skittles"];
 
 var addElementToBeginningOfArray(array, element) = ["New york", "London"]
 
-["Kent",...addElementToBeginningOfArray]
+addElementToBeginningOfArray.push ("Kent")
