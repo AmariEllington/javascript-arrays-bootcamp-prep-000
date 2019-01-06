79882d6152b55549c8d3c4a68363df3569f6fc2a
Amari Ellington <amari.l.ellington@gmail.com>
2019-01-06 14:04:46 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,11 +1,3 @@
 var chocolateBars = ["snickers","hundred grand","kitkat","skittles"];
-var candyString = 'foo';
 
-function addElementToBeginningOfArray(chocolateBars, candyString){
-return [candyString, …chocolateBars];
-}
 
-function destructivelyAddElementToBeginningOfArray(chocolateBars, candyString){
-chocolateBars.unshift(candyString);
-return chocolateBars;
-}
